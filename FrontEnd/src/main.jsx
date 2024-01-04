b9f806da02d614469fa49db20bb24f28cb880af8
Andreea Ghenea <andreeag@Andreeas-MBP.cust.communityfibre.co.uk>
2024-01-04 19:48:14 +0000

    Added working Stripe Payment

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -5,11 +5,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap//dist/js/bootstrap.min.js";
 import App from './App.jsx';
 import './index.css'
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+
+const stripeKey = import.meta.env.VITE_STRIPE_KEY;
+const stripePromise = loadStripe(stripeKey);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Elements stripe={stripePromise}>
+        <App />
+      </Elements>
     </Router>
   </React.StrictMode>,
 )
