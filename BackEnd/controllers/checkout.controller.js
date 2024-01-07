1ad9359728a9c55beec6857e85df6346311cbfbb
Andreea Ghenea <andreeag@Andreeas-MBP.cust.communityfibre.co.uk>
2024-01-07 19:46:10 +0000

    Fixed minor issue within checkout process (price decimals)

--- BackEnd/controllers/checkout.controller.js
+++ BackEnd/controllers/checkout.controller.js
@@ -24,7 +24,7 @@ export const createCheckoutSession = async (req, res) => {
                             name: item.name,
                             images: [item.image],
                         },
-                        unit_amount: item.price * 100,
+                        unit_amount: Math.round(item.price * 100),
                     },
                     quantity: item.quantity
                 }
